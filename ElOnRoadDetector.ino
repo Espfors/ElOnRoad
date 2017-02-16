48fdd4f442ded35ce95534e9a8acc24ff8edf806
Espfors <nespfors@gmail.com>
2017-02-16 14:31:27 +0100

    Add files via upload

---
+++ ElOnRoadDetector.ino
@@ -0,0 +1,26 @@
+int inputarray[10];
+int in = A8;
+
+
+void setup() {
+  Serial.begin(9600);
+  /*for (int i: inputarray) {
+    pinMode(i, INPUT);
+  }*/
+  pinMode(A8,INPUT);
+}
+
+void loop() {
+  /*for(int i: inputarray) {
+    inputarray[i] = analogRead(i);
+  }*/
+  testOneSensor();
+
+  delay(50);
+}
+
+void testOneSensor() {
+  int val = analogRead(in);
+  Serial.println(val);  
+}
+
