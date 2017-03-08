f0be510437730d04d22e91deb27dcaa6aba03e33
Espfors <nespfors@gmail.com>
2017-03-08 10:49:28 +0100

    Add files via upload
    Added template of filter

--- Prototype_1.ino
+++ Prototype_1.ino
@@ -2,10 +2,12 @@ int sensor[48]; // Vector containing Sensor value, four values for each sensor
 int average[12]; // Vector containing the averager of the four latest sensor value for each sensor
 int posIndex; // Index of the sensor detecting the road
 int maxValue; // Value of the sensor detecting the road
+int oldavg[12];
 int pos; // used for iterating of sensor[48]vector
-
+int motorPin = 17;
 void setup() {
-  Serial.begin(9600); // Initiates the serial communication 
+  Serial.begin(9600); // Initiates the serial communication for debugging
+  
   // Setup digitalpin 0 to 11 as output, will be used to give feedback where the system findes the road
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
@@ -20,18 +22,18 @@ void setup() {
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   // Setup analogpin A0 to A11 as input, will be used for reading the IR-sensors
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
-  pinMode(A3,INPUT);
-  pinMode(A4,INPUT);
-  pinMode(A5,INPUT);
-  pinMode(A6,INPUT);
-  pinMode(A7,INPUT);
-  pinMode(A8,INPUT);
-  pinMode(A9,INPUT);
-  pinMode(A10,INPUT);
-  pinMode(A11,INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+  pinMode(A8, INPUT);
+  pinMode(A9, INPUT);
+  pinMode(A10, INPUT);
+  pinMode(A11, INPUT);
   posIndex = 0; // Initiates index
   pos = 36; // Initiates vector index
 }
@@ -77,21 +79,26 @@ void loop() {
   while (true) { //Loop used for identification
     maxValue = 0; // resets max value
     sensor[pos] = analogRead(A0);
-    sensor[pos+1] = analogRead(A1);
-    sensor[pos+2] = analogRead(A2);
-    sensor[pos+3] = analogRead(A3);
-    sensor[pos+4] = analogRead(A4);
-    sensor[pos+5] = analogRead(A5);
-    sensor[pos+6] = analogRead(A6);
-    sensor[pos+7] = analogRead(A7);
-    sensor[pos+8] = analogRead(A8);
-    sensor[pos+9] = analogRead(A9);
-    sensor[pos+10] = analogRead(A10);
-    sensor[pos+11] = analogRead(A11);
+    sensor[pos + 1] = analogRead(A1);
+    sensor[pos + 2] = analogRead(A2);
+    sensor[pos + 3] = analogRead(A3);
+    sensor[pos + 4] = analogRead(A4);
+    sensor[pos + 5] = analogRead(A5);
+    sensor[pos + 6] = analogRead(A6);
+    sensor[pos + 7] = analogRead(A7);
+    sensor[pos + 8] = analogRead(A8);
+    sensor[pos + 9] = analogRead(A9);
+    sensor[pos + 10] = analogRead(A10);
+    sensor[pos + 11] = analogRead(A11);
     digitalWrite(posIndex, LOW); // Turns the identifications led off
-    for (int j = 0; j < 12; j++){
-          average[j]= ((sensor[j]+sensor[j+12]+sensor[j+24]+sensor[j+36])>>2); // calculate average
+    for (int j = 0; j < 12; j++) {
+      average[j] = ((sensor[j] + sensor[j + 12] + sensor[j + 24] + sensor[j + 36]) >> 2); // calculate average
+      if (average[j] > oldavg[j]+100 || average[j] < oldavg-100) { //Disregard if new average is much larger or smaller then old average, +-100 is a just a guess.
+        average[j]=oldavg[j];
+      } else {
+        oldavg[j] = average[j];
       }
+    }
     //Finds max value and index of max value
     for (int i = 0; i < 12; i++) {
       if (average[i] > maxValue) {
@@ -101,8 +108,9 @@ void loop() {
     }
     Serial.println("Road at sensor: " + posIndex); // Print index to serial monitor
     digitalWrite(posIndex, HIGH); // Turns the identifications led on
-    pos = (pos+12)%48; // increase pos by 12 module modulus 48
+    pos = (pos + 12) % 48; // increase pos by 12 module modulus 48
   }
+  delay(50);
 }
 
 
